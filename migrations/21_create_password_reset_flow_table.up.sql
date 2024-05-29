f70985ce5dc201acf4688d4fff2de57cd636163b
JINJI <countdastars@naver.com>
2024-05-30 00:09:27 +0900

    feat: Add password reset flow model, Add migration

---
+++ migrations/21_create_password_reset_flow_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE password_reset_flows (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    flow_id VARCHAR(255) UNIQUE NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT fk_password_reset_flows_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
